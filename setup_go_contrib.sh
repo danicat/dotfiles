64dcc4c0b5cbddec603e6edd39dcbedfc906faab
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2019-05-24 12:12:26 +0100

    clone golang repo

--- setup_go_contrib.sh
+++ setup_go_contrib.sh
@@ -2,3 +2,7 @@ sudo apt-get install git -y
 go get -u golang.org/x/tools/cmd/go-contrib-init
 go get -u golang.org/x/review/git-codereview
 sudo ln -s ~/go/bin/git-codereview `git --exec-path`
+mkdir $HOME/golang
+cd $HOME/golang/
+git clone --depth=1 https://go.googlesource.com/go
+cd -
